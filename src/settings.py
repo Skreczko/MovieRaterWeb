69c236629765dd8c80d09021f6ac9580e30a9abf
Dawid Skreczko <dawid.skreczko@gmail.com>
2018-12-09 18:29:13 +0100

    repaired 404 error for not logged users v3

--- src/settings.py
+++ src/settings.py
@@ -130,7 +130,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/2.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'EN-US'
 
 TIME_ZONE = 'UTC'
 
@@ -146,7 +146,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, "static")
+#STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
