d691e92d33dc94345ae22ef2ab77e497d20e4647
Dawid Skreczko <dawid.skreczko@gmail.com>
2018-12-13 23:54:21 +0100

    added ordering in movie sectionv10

--- actors/models.py
+++ actors/models.py
@@ -169,7 +169,7 @@ class CrewRole(models.Model):
 		return str("{movie} - {actor} - {role}".format(movie=self.movie, actor=self.actor, role=self.role))
 
 	class Meta:
-		ordering = ('role', 'actor', 'movie')
+		ordering = ('actor', 'movie')
 
 
 def slug_create(instance, new_slug=None):
